27bf91f4776f326f804e187cfed34a789517c6ec
Naresh Kumar H <nareshkumarh@live.com>
2024-03-12 14:11:27 +0530

    updated

--- routes/upload.js
+++ routes/upload.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const { UploadController } = require("../controllers/upload")
 
 const router = Router()
-
+router.get("/", (req, res) => res.send("Express on Vercel"));
 router.post("/uploads/initializeMultipartUpload", UploadController.initializeMultipartUpload)
 router.post("/uploads/getMultipartPreSignedUrls", UploadController.getMultipartPreSignedUrls)
 router.post("/uploads/finalizeMultipartUpload", UploadController.finalizeMultipartUpload)
